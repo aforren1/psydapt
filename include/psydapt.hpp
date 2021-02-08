2277923eb603f2d4a63fccb01f39b0f9bd278e55
Alex Forrence <alex.forrence@gmail.com>
2021-02-07 21:59:07 -0500

    don't rely on version yet

--- include/psydapt.hpp
+++ include/psydapt.hpp
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with psydapt.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "config.hpp"
+//#include "config.hpp"
 #include "psy_incl/staircase/staircase.hpp"
 #include "psy_incl/questplus/weibull.hpp"
 #include "psy_incl/questplus/norm_cdf.hpp"
