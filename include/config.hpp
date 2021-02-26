fd39ede0142f8783f881c4430baaf3fef531d5d1
Alex Forrence <alex.forrence@gmail.com>
2021-02-25 22:32:28 -0500

    try to implement throw macro for emscripten

--- include/config.hpp
+++ include/config.hpp
@@ -6,12 +6,19 @@
 #define PSYDAPT_VERSION_PATCH 4
 
 #if defined(PSYDAPT_DISABLE_EXCEPTIONS)
+#if defined(__EMSCRIPTEN__)
+#define PSYDAPT_THROW(_, msg)  \
+    {                          \
+        EM_ASM({ throw msg; }) \
+    }
+#else
 #include <iostream>
 #define PSYDAPT_THROW(_, msg)          \
     {                                  \
         std::cerr << msg << std::endl; \
         std::abort();                  \
     }
+#endif
 #else
 #define PSYDAPT_THROW(exception, msg) throw exception(msg)
 #endif
